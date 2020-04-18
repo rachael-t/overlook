aa9b9ad20071711a0d2c17cf38f51d3922714060
Rachael Thomas <rnthomas001@gmail.com>
2020-04-18 15:47:41 -0600

    Add DOM manipulation method to display previous bookings

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,9 +1,26 @@
 import $ from 'jquery';
 
+
 const domUpdates = {
   displayCustomerName(name) {
     $('#customer-welcome').text(`Welcome back ${name}!`);
+  },
+
+  displayCustomerBookings(bookings) {
+    $('.rooms-to-book-container').html('');
+    bookings.forEach(booking => {
+      $('.rooms-to-book-container').append(
+        `
+          <li class="room-card" id="${booking.id}">
+            <p class="room-card-title">Room ${booking.roomNumber}</p>
+            <div class="room-card-details-container">
+                <p class="room-card-details">Date of Reservation:</br>${booking.date}</p>
+            </div>
+          </li>
+      `)
+    })
   }
+
 };
 
 export default domUpdates;
