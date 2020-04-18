ec5f81b52eec0c2eac974fffc13d80d60479f479
Rachael Thomas <rnthomas001@gmail.com>
2020-04-18 11:31:31 -0600

    Add planned properties and methods to parent class

--- src/User.js
+++ src/User.js
@@ -2,9 +2,32 @@ import domUpdates from './domUpdates.js'
 import Fetcher from './Fetcher.js'
 
 class User {
-  constructor() {
-    
+  constructor(usersData, roomsData, bookingsData, id) {
+    this.users = usersData;
+    this.rooms = roomsData;
+    this.bookings = bookingsData;
+    this.customer = null;
   }
+
+  // getCustomerData(id) {
+  //
+  // },
+  //
+  // getCustomerBookings(id) {
+  //
+  // },
+  //
+  // getCustomerAmountSpent(id) {
+  //
+  // },
+  //
+  // makeCustomerBooking(date, roomNum) {
+  //
+  // },
+  //
+  // getRoomsAvailable(date) {
+  //
+  // },
 }
 
 export default User;
