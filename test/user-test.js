c372da290bf91cf3fcb4dd35245e51c50e338a89
Rachael Thomas <rnthomas001@gmail.com>
2020-04-18 14:50:26 -0600

    Write initial test for getCustomerBookings

--- test/user-test.js
+++ test/user-test.js
@@ -43,5 +43,24 @@ describe('User', function () {
     expect(domUpdates.displayCustomerName).to.have.been.called.with("Leatha Ullrich");
   })
 
+  it('should get all bookings unique to a customer', function() {
+    expect(user.getCustomerBookings(1)).to.deep.equal([
+      {
+      "id": "5fwrgu4i7k55hl6t8",
+      "userID": 1,
+      "date": "2020/02/05",
+      "roomNumber": 6,
+      "roomServiceCharges": []
+      },
+      {
+      "id": "5fwrgu4i7k55hl6td",
+      "userID": 1,
+      "date": "2020/01/31",
+      "roomNumber": 10,
+      "roomServiceCharges": []
+      }
+    ]);
+  })
+
 
 })
