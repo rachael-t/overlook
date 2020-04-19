f201bd056238e1d20a2c9e036e5f9e3624b1c3e9
Rachael Thomas <rnthomas001@gmail.com>
2020-04-19 14:50:03 -0600

    Create date picker DOM manipulation methods

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,7 +1,25 @@
 import $ from 'jquery';
-
+import flatpickr from "flatpickr";
 
 const domUpdates = {
+  addDatePicker() {
+    flatpickr("#datepicker", {
+      dateFormat: "Y/m/d"
+    });
+  },
+
+  displayDatePicker() {
+    $('.customer-filter-date').html('');
+    $('.message-banner').text(`Please select a date:`);
+    $('.customer-filter-date').prepend(`<input class="customer-select-date" id="datepicker" placeholder="Select Date"/>`);
+    this.addDatePicker();
+  },
+
+  clearDatePicker() {
+    $('.message-banner').html('');
+    $('.customer-filter-date').html('');
+  },
+
   displayCustomerName(name) {
     $('.message-banner').text(`Welcome back ${name}!`);
   },
@@ -19,6 +37,9 @@ const domUpdates = {
           </li>
       `)
     })
+    if (bookings.length === 0) {
+      $('.message-banner').text(`You do not have any upcoming reservations.`);
+    }
   },
 
   displayCustomerAmountSpent(amount) {
@@ -30,7 +51,25 @@ const domUpdates = {
   displayRoomsAvailable(rooms) {
     let numAvailable = rooms.length;
     $('.rooms-to-book-container').html('');
-    $('.message-banner').text(`There are ${numAvailable} rooms available today.`);
+    $('.message-banner').text(`There are ${numAvailable} rooms available.`);
+    rooms.forEach(room => {
+      $('.rooms-to-book-container').append(
+        `
+            <li class="room-card" id="${room.number}">
+              <p class="room-card-title">Room ${room.number}</p>
+              <div class="room-card-details-container">
+                  <p class="room-card-details">Room Type: ${room.roomType}</p>
+                  <p class="room-card-details">Does it Have a Bidet: ${room.bidet}</p>
+                  <p class="room-card-details">Bed Size: ${room.bedSize}</p>
+                  <p class="room-card-details">Number of Beds: ${room.numBeds}</p>
+                  <p class="room-card-details">Cost Per Night: $${room.costPerNight}</p>
+                  <button type="submit" name="button" class="book-room-btn">Book Room
+                  </button>
+              </div>
+            </li>
+        `
+      )
+    })
   },
 
   displayTodaysRevenue(amount) {
@@ -42,7 +81,7 @@ const domUpdates = {
   displayTodaysOccupancy(occupancy) {
     $('.rooms-to-book-container').html('');
     $('.message-banner').text(`Riverside Resort is ${occupancy}% occupied today.`);
-  }
+  },
 
 };
 
