9f037ac5de6769b6c19edaa5fb0f33b75aa763d3
Rachael Thomas <rnthomas001@gmail.com>
2020-04-19 17:40:35 -0600

    Refactor Fetcher from a class to an object. Add post functionality for booking a room

--- src/Fetcher.js
+++ src/Fetcher.js
@@ -1,21 +1,38 @@
-class Fetcher {
-  constructor() {}
-
+const fetcher = {
   fetchUsersData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
       .then(response => response.json())
-  }
+  },
 
   fetchRoomsData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
       .then(response => response.json())
-  }
+  },
 
   fetchBookingsData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
       .then(response => response.json())
+  },
+
+  postReservation(usersID, dateRequested, room) {
+    let body = {
+      'userID': usersID,
+      'date': dateRequested,
+      'roomNumber': room
+    }
+    fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
+      .then(data => console.log("success", data))
+      .catch(err => console.log("error", err.message))
   }
+
 
 }
 
-export default Fetcher;
+export default fetcher;
