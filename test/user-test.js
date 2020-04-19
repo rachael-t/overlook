08aaaea05d836748b9a1cd1f45d7b59073d0baa8
Rachael Thomas <rnthomas001@gmail.com>
2020-04-19 11:00:04 -0600

    Add spy testing on domUpdates displayRoomsAvailable

--- test/user-test.js
+++ test/user-test.js
@@ -16,7 +16,7 @@ describe('User', function () {
 
   beforeEach(() => {
     user = new User(usersTestData, roomsTestData, bookingsTestData);
-    chai.spy.on(domUpdates, ['displayCustomerName', 'displayCustomerBookings', 'displayCustomerAmountSpent'], () => true)
+    chai.spy.on(domUpdates, ['displayCustomerName', 'displayCustomerBookings', 'displayCustomerAmountSpent', 'displayRoomsAvailable'], () => true)
   });
 
   afterEach(function() {
@@ -96,8 +96,81 @@ describe('User', function () {
 
   it('should return the total rooms available for the current date', function() {
     expect(user.getRoomsAvailable("2020/02/14")).to.equal(9);
-    // expect(domUpdates.displayCustomerAmountSpent).to.have.been.called(1);
-    // expect(domUpdates.displayCustomerAmountSpent).to.have.been.called.with(722.60);
+    expect(domUpdates.displayRoomsAvailable).to.have.been.called(1);
+    expect(domUpdates.displayRoomsAvailable).to.have.been.called.with([
+        {
+        "number": 1,
+        "roomType": "residential suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 358.4
+        },
+        {
+        "number": 2,
+        "roomType": "suite",
+        "bidet": false,
+        "bedSize": "full",
+        "numBeds": 2,
+        "costPerNight": 477.38
+        },
+        {
+        "number": 3,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
+        },
+        {
+        "number": 4,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 429.44
+        },
+        {
+        "number": 5,
+        "roomType": "single room",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 2,
+        "costPerNight": 340.17
+        },
+        {
+        "number": 6,
+        "roomType": "junior suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 397.02
+        },
+        {
+        "number": 8,
+        "roomType": "junior suite",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 261.26
+        },
+        {
+        "number": 9,
+        "roomType": "single room",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 200.39
+        },
+        {
+        "number": 10,
+        "roomType": "suite",
+        "bidet": false,
+        "bedSize": "twin",
+        "numBeds": 1,
+        "costPerNight": 497.64
+        }
+    ]);
   })
 
 })
