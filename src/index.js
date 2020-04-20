fe655961fcd654d28b5208170fb951476d6b4e74
Rachael Thomas <rnthomas001@gmail.com>
2020-04-20 11:41:55 -0600

    Add cancellation handler

--- src/index.js
+++ src/index.js
@@ -18,20 +18,23 @@ let user;
 // Event listeners
 $('body').on('click', '#sign-in-button', logUserIn);
 $('body').on('click', '.logout-btn', logUserOut);
+
   // Manager page event listners
 $('#customer-name-selection').change(function() {
-  let grabbedId = $(this).children(":selected").attr("id");
+  let grabbedId = $(this).children(':selected').attr('id');
   searchedUserId = parseInt(grabbedId);
-});
-$('body').on('click', '#customer-search-button', searchUserInfo);
+  });
+$('body').on('click', '#customer-search-button', searchUserHandler);
 $('body').on('click', '#manager-rooms-available', roomsAvailableHandler);
 $('body').on('click', '#manager-todays-revenue', todaysRevenueHandler);
 $('body').on('click', '#manager-todays-occupation', todaysOccupationHandler);
+$('body').on('click', '#manager-cancellation', cancellationPageHandler);
+
   // Customer event liseners
 $('body').on('click', '#customer-make-reservation', makeReservationHandler);
-$('body').on('change', "#datepicker", reservationDateHandler);
+$('body').on('change', '#datepicker', reservationDateHandler);
 $('body').on('change', '.room-type-selection', reservationFilterHandler);
-$('body').on('click', ".book-room-btn", requestBooking);
+$('body').on('click', '.book-room-btn', requestBooking);
 $('body').on('click', '#customer-past-reservations', pastReservationsHandler);
 $('body').on('click', '#customer-upcoming-resverations', upcomingReservationsHandler);
 $('body').on('click', '#customer-total-spent', totalSpentHandler);
@@ -92,12 +95,13 @@ function logUserOut() {
 };
 
 // Manager page
-function searchUserInfo() {
+function searchUserHandler() {
   user.getCustomerData(searchedUserId);
   let name = $("#customer-name-selection").val();
   let amount = manager.getCustomerAmountSpent(searchedUserId).toFixed(2);
   let allBookings = manager.getCustomerBookings(searchedUserId, today, 'all').length;
   domUpdates.displayCustomerDetails(name, allBookings, amount);
+  domUpdates.addCancellationToMenuBar();
 };
 
 function roomsAvailableHandler() {
@@ -112,10 +116,15 @@ function todaysOccupationHandler() {
   manager.getTodaysOccupancy(today);
 };
 
+function cancellationPageHandler() {
+  let futureBookings = manager.getCustomerBookings(searchedUserId, today, 'future');
+  domUpdates.displayCancellationOptions(futureBookings);
+};
+
 // Customer page
 function loadCustomerInfo(customerID) {
   customer = user.getCustomerData(customerID);
-}
+};
 
 function makeReservationHandler() {
   domUpdates.displayDatePicker();
